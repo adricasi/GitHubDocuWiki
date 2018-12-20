ee2f48a6c9f5a2111fd099b046cac568ea1ea914
adricasi <adricasi@users.noreply.github.com>
2018-12-20 18:34:53 +0100

    First commit

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$WT5ZiL6l$SSblEuSyNDt5nQAJ4iCiy0:admin:adriator6@gmail.com:admin,user
